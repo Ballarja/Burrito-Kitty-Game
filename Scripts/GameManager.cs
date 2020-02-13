01c5736c4a44b7fdc638087f9e9ab22355f1166c
Ballarja <54514754+Ballarja@users.noreply.github.com>
2020-02-12 18:53:21 -0600

    Script Files added

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    //references
+    public GameObject gameOverCanvas;
+    public GameObject score;
+    public GameObject getReadyImg;
+    public GameObject pauseBtn;
+    public Animator blackFadeAnim;
+
+    public static Vector2 bottomLeft;
+
+    //game states
+    public static bool gameOver;
+    public static bool gameHasStarted;
+    public static bool gameIsPaused;
+
+    public Text panelScore;
+
+    public static int gameScore;
+    int drawScore;
+
+
+    private void Awake()
+    {
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        gameHasStarted = false;
+        gameIsPaused = false;
+    }
+
+    public void GameHasStarted()
+    {
+        gameHasStarted = true;
+        score.SetActive(true);
+        getReadyImg.SetActive(false);
+        pauseBtn.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        gameScore = score.GetComponent<Score>().GetScore();
+        score.SetActive(false);
+        Invoke("ActivateGameOverCanvas", 1);
+        pauseBtn.SetActive(false);
+    }
+
+    void ActivateGameOverCanvas()
+    {
+        gameOverCanvas.SetActive(true);
+        AudioManager.audiomanager.Play("die");
+    }
+
+
+    public void OnOkBtnPressed()
+    {
+        AudioManager.audiomanager.Play("transition");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void OnMenuBtnPressed()
+    {
+        AudioManager.audiomanager.Play("transition");
+        //SceneManager.LoadScene("Menu");
+        blackFadeAnim.SetTrigger("fadeIn");
+    }
+
+
+    public void DrawScore()
+    {
+        if (drawScore <= gameScore)
+        {
+            panelScore.text = drawScore.ToString();
+            drawScore++;
+            Invoke("DrawScore", 0.05f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Scripts/Kitty.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kitty : MonoBehaviour
+{
+    //references
+    public Score score;
+    public GameManager gameManager;
+    public Sprite kittyDied;
+    public ColumnSpawner columnSpawner;
+    public Animator kittyParentAnim;
+    public Animator getReadyAnim;
+    public Animator hitEffect;
+    public Animator cameraAnim;
+
+    SpriteRenderer sp;
+    Animator anim;
+    Rigidbody2D rb;
+    public float speed;
+
+
+    int angle;
+    int maxAngle = 20;
+    int minAngle = -90;
+
+    bool touchedGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        rb.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) &&
+            GameManager.gameOver == false &&
+            GameManager.gameIsPaused == false)
+        {
+            if (GameManager.gameHasStarted == false)
+            {
+                rb.gravityScale = 0.8f;
+                kittyParentAnim.enabled = false;
+                Flap();
+                //column Spawner
+                //set the trigger for the get ready anim
+                getReadyAnim.SetTrigger("fadeOut");
+            }
+            else
+            {
+                Flap();
+            }
+
+
+        }
+
+        KittyRotation();
+
+
+    }
+
+    public void OnGetReadyAnimFinished()
+    {
+        columnSpawner.InstantiateColumn();
+        gameManager.GameHasStarted();
+    }
+
+    void Flap()
+    {
+        AudioManager.audiomanager.Play("flap");
+        rb.velocity = Vector2.zero;
+        rb.velocity = new Vector2(rb.velocity.x, speed);
+    }
+
+    void KittyRotation()
+    {
+        if (rb.velocity.y > 0)
+        {
+            rb.gravityScale = 0.8f;
+
+            if (angle <= maxAngle)
+            {
+                angle = angle + 4;
+            }
+        }
+        else if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = 0.6f;
+
+            if (rb.velocity.y < -1.3f)
+            {
+                if (angle >= minAngle)
+                {
+                    angle = angle - 3;
+                }
+            }
+
+        }
+
+        if (touchedGround == false)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (GameManager.gameOver == false)
+        {
+            if (collision.CompareTag("Column"))
+            {
+                //print("We have scored");
+                AudioManager.audiomanager.Play("point");
+                score.Scored();
+            }
+            else if (collision.CompareTag("Pipe"))
+            {
+                KittyDieEffect();
+                //game over
+                gameManager.GameOver();
+            }
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (GameManager.gameOver == false)
+            {
+                KittyDieEffect();
+                //game over
+                gameManager.GameOver();
+                GameOver();
+
+            }
+            else
+            {
+
+                GameOver();
+            }
+
+        }
+    }
+
+    void KittyDieEffect()
+    {
+        AudioManager.audiomanager.Play("hit");
+        hitEffect.SetTrigger("hit");
+        cameraAnim.SetTrigger("shake");
+    }
+
+    void GameOver()
+    {
+        touchedGround = true;
+        anim.enabled = false;
+        sp.sprite = kittyDied;
+        transform.rotation = Quaternion.Euler(0, 0, -90);
+    }
+
+
+}
